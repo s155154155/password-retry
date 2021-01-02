f05ad87a806ccae32b952068b6323e3b01184bef
NomikLee <nomiklee@liyuanhaode-MacBook-Pro.local>
2021-01-02 16:45:57 +0800

    up code

--- pw.py
+++ pw.py
@@ -3,11 +3,14 @@
 password = 'a123456'
 cot = 3
 while cot > 0:
+	cot = cot - 1
 	pw = input('請輸入正確密碼 最多可輸入3次')
 	if pw == password:
 		print('登入成功')
 		break
 	else:
-		cot = cot - 1
-		print('密碼錯誤還有', cot,'次機會')
-
+		print('密碼錯誤')
+		if cot > 0:
+			print('還有', cot,'次機會')
+		else:
+			print('密碼已鎖住')
