e28b773a71c47c5a4a084fc44cee25305a9f5e13
NomikLee <nomiklee@liyuanhaode-MacBook-Pro.local>
2021-01-02 16:31:02 +0800

    upload pw

---
+++ pw.py
@@ -0,0 +1,15 @@
+#密碼蟲試程式
+
+password = 'a123456'
+cot = 3
+while True:
+	pw = input('請輸入正確密碼 最多可輸入3次')
+	if pw == password:
+		print('登入成功')
+		break
+	else:
+		cot = cot - 1
+		print('密碼錯誤還有', cot,'次機會')
+		if cot == 0:
+			break
+
